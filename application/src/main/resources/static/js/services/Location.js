569fb2bbea23b8a9251f9229bf6eaf1e7093e861
Venkata Jaswanth <aj.jaswanth@gmail.com>
2017-10-04 23:17:10 +0530

    Fix location url

--- application/src/main/resources/static/js/services/Location.js
+++ application/src/main/resources/static/js/services/Location.js
@@ -2,7 +2,7 @@
 	angular.module('openDoor').factory('Location', function($http) {
 
 		var get = function() {
-			return $http.get('http://localhost:8080/locations').then(function(response) {
+			return $http.get('/locations').then(function(response) {
 				return response.data;
 			});
 		};
