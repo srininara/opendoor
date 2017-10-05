3f1038a7399d690db978f5750d2f6b2542dfae0a
Vinoth <vkuppusamy@aconex.com>
2017-10-05 17:50:30 +0530

    Display the up vote and down vote

--- application/src/main/resources/static/js/component/feedback/feedbackCtrl.js
+++ application/src/main/resources/static/js/component/feedback/feedbackCtrl.js
@@ -1,8 +1,20 @@
 (function(angular) {
 	angular.module('openDoor').controller('feedbackCtrl', ['Feedback', function(Feedback) {
 		var ctrl = this;
-		Feedback.getAll().then(function (feedback) {
+		Feedback.getAll().then(function(feedback) {
 			ctrl.feedback = feedback;
 		});
+
+		ctrl.upVote = function(currentFeedback) {
+			// TODO: Save the up vote
+			currentFeedback.upVote = currentFeedback.upVote || 0;
+			currentFeedback.upVote += 1;
+		};
+
+		ctrl.downVote = function(currentFeedback) {
+			// TODO: Save the up vote
+			currentFeedback.downVote = currentFeedback.downVote || 0;
+			currentFeedback.downVote += 1;
+		};
 	}]);
 })(this.angular);
