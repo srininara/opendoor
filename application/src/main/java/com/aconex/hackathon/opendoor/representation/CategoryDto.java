d7729e4789cf2a6beebe767db41f1d30bab0844d
Ranganathan <rbalashanmugam@aconex.com>
2017-10-05 19:34:55 +0530

    Fix duplicate constructor in CategoryDto

--- application/src/main/java/com/aconex/hackathon/opendoor/representation/CategoryDto.java
+++ application/src/main/java/com/aconex/hackathon/opendoor/representation/CategoryDto.java
@@ -19,31 +19,5 @@ public class CategoryDto {
 
     private String fullDescription;
 
-    public CategoryDto(int id, String name, String icon, String shortDescription, String fullDescription) {
-        this.id = id;
-        this.name = name;
-        this.icon = icon;
-        this.shortDescription = shortDescription;
-        this.fullDescription = fullDescription;
-    }
 
-    public String getIcon() {
-        return icon;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
-    public String getFullDescription() {
-        return fullDescription;
-    }
 }
