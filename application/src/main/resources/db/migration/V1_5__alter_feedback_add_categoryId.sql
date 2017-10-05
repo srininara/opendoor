6653c838b9f5bf07f3f532f3aac2f032ee89c8de
Sowmya A <sowmyaa@aconex.com>
2017-10-05 12:26:24 +0530

    Removing not null constraint for category

--- application/src/main/resources/db/migration/V1_5__alter_feedback_add_categoryId.sql
+++ application/src/main/resources/db/migration/V1_5__alter_feedback_add_categoryId.sql
@@ -1,2 +1,2 @@
 alter table feedback
-    add column category_id int not null, add foreign key (category_id) REFERENCES category(id);
+    add column category_id int, add foreign key (category_id) REFERENCES category(id);
